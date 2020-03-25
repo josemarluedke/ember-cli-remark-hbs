61a95217c74f89aa23536cb9ab83abe9846ff549
Josemar Luedke <josemarluedke@gmail.com>
2020-03-24 18:59:40 -0700

    Initial implementation

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'parent') {
+      let options = this.app.options['ember-cli-remark'];
+      let RemarkTemplateCompiler = require('./lib/compiler');
+      registry.add('template', new RemarkTemplateCompiler(options));
+    }
+  }
 };
